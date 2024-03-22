d505bdaf57a991564f0f0414b5b7ca6af1b30a2b
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-03-22 16:16:11 +0800

    upd

--- packages/saas-backend/src/main.ts
+++ packages/saas-backend/src/main.ts
@@ -1,7 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+// import { AppModule } from './app.module';
+import fs from "fs";
 
 async function bootstrap() {
+  // 获取启动变量，动态修改.env文件
+  const CFG = process.env.CFG;
+  if (CFG) {
+    console.log('CFG', CFG);
+    fs.writeFileSync('./.env', CFG, 'utf8');
+  }
+  const { AppModule } = await import('./app.module');
   const app = await NestFactory.create(AppModule);
   await app.listen(8080);
 }

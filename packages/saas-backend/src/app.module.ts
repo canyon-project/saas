8e363b81ce97e42ca5a5919b4ba54cee34cd8e1f
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-03-22 18:05:08 +0800

    feat: upd

--- packages/saas-backend/src/app.module.ts
+++ packages/saas-backend/src/app.module.ts
@@ -1,15 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {PrismaModule} from "./prisma/prisma.module";
-import {join} from "path";
+import { PrismaModule } from './prisma/prisma.module';
+import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [PrismaModule,
+  imports: [
+    PrismaModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../../saas-platform', 'dist'),
       exclude: ['/graphql/(.*)'],
-    }),],
+    }),
+    ConfigModule.forRoot({
+      envFilePath: './.[env]',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
